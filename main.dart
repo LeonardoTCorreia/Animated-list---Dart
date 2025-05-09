da590dc4500d0c3ecde10f047ac21d1af547d7a0
Leonardo Correia <135469522+LeonardoTCorreia@users.noreply.github.com>
2025-05-08 21:26:10 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,102 +1,143 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Counter()),
+        ChangeNotifierProvider(create: (_) => ItemList()), // Move para o escopo global
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class Counter extends ChangeNotifier {
+  int _count = 0;
+
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners();
+  }
+}
+
+class ItemList extends ChangeNotifier {
+  final List<String> _items = ['banana', 'maçã', 'uva'];
+
+  List<String> get items => _items;
+
+  void addItem(String item) {
+    _items.add(item);
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Animated ListView',
+      title: 'Multi Provider App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      home: const AnimatedListViewScreen(),
+      home: const HomePage(),
     );
   }
 }
 
-class AnimatedListViewScreen extends StatefulWidget {
-  const AnimatedListViewScreen({Key? key}) : super(key: key);
-
-  @override
-  State<AnimatedListViewScreen> createState() => _AnimatedListViewScreenState();
-}
-
-class _AnimatedListViewScreenState extends State<AnimatedListViewScreen> {
-  final List<String> _items = [];
-  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
-  int _counter = 1;
-
-  void _addItem() {
-    final index = _items.length; // Posição onde o item será adicionado
-    final newItem = 'Item $_counter';
-    _items.add(newItem);
-    _counter++;
-
-    // Adiciona o item com animação
-    _listKey.currentState?.insertItem(
-      index,
-      duration: const Duration(milliseconds: 500),
-    );
-  }
-
-  Widget _buildItem(
-    BuildContext context,
-    int index,
-    Animation<double> animation,
-  ) {
-    return SizeTransition(
-      sizeFactor: animation,
-      child: Card(
-        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        child: ListTile(
-          title: Text(_items[index]),
-          trailing: IconButton(
-            icon: const Icon(Icons.delete, color: Colors.red),
-            onPressed: () => _removeItem(index),
-          ),
-        ),
-      ),
-    );
-  }
-
-  void _removeItem(int index) {
-    final removedItem = _items[index];
-    _items.removeAt(index);
-
-    // Remove o item com animação
-    _listKey.currentState?.removeItem(
-      index,
-      (context, animation) => SizeTransition(
-        sizeFactor: animation,
-        child: Card(
-          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-          child: ListTile(
-            title: Text(removedItem),
-          ),
-        ),
-      ),
-      duration: const Duration(milliseconds: 500),
-    );
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final counter = Provider.of<Counter>(context);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Animated ListView'),
+        title: Text('Multi Provider App (${counter.count})'),
       ),
-      body: AnimatedList(
-        key: _listKey,
-        initialItemCount: _items.length,
-        itemBuilder: _buildItem,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            Text(
+              '${counter.count}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => const SecondPage(),
+                  ),
+                );
+              },
+              child: const Text('nova tela'),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _addItem,
+        onPressed: counter.increment,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final counter = Provider.of<Counter>(context);
+    final itemList = Provider.of<ItemList>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Multi Provider App (${counter.count})'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: Column(
+        children: [
+          Text('Contador: ${counter.count}'),
+          Expanded(
+            child: AnimatedList(
+              initialItemCount: itemList.items.length,
+              itemBuilder: (context, index, animation) {
+                return SlideTransition(
+                  position: animation.drive(
+                    Tween<Offset>(
+                      begin: const Offset(-1, 0),
+                      end: Offset.zero,
+                    ).chain(CurveTween(curve: Curves.easeInOut)),
+                  ),
+                  child: ListTile(
+                    title: Text(itemList.items[index]),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final newItem = 'novo item ❤️';
+          itemList.addItem(newItem);
+          AnimatedList.of(context).insertItem(itemList.items.length - 1);
+        },
         child: const Icon(Icons.add),
       ),
     );
